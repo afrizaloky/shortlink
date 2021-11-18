42dbb10535f18822d91cca2ae080bcbdd978e465
afrizaloky <afrizaloky@outlook.com>
2021-11-18 16:47:56 +0700

    add console

--- src/db.ts
+++ src/db.ts
@@ -5,6 +5,9 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 
 const db: IMonkManager = monk.default(process.env.MONGO_URI ?? '')
+// console.log(db._state)
+console.log(process.env.MONGO_URI)
+
 
 const urls: ICollection = db.get('urls')
 urls.createIndex({"slug": 1}, {unique: true})
